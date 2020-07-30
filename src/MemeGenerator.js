ee62e3b68f4ae32ae4bf9c24102d759d79d7aaa1
Peter <peterpauco1@gmail.com>
2020-07-30 15:07:07 +0200

    edited

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import Draggable, {DraggableCore} from 'react-draggable';
+import Draggable from 'react-draggable';
 
 class MemeGenerator extends Component {
     constructor() {
         super();
         this.state={
-            topText:"",
+            topText:"koko",
             bottomText: "",
             randomImg: "https://i.ytimg.com/vi/Nws_Vc1fY5o/maxresdefault.jpg"    ,   
             allMemeImgs: [],
             deltaPosition: {
                 x: 0, y: 0
-              },
-              controlledPosition: {
-                x: -400, y: 200
               }
         };
         this.handleChange = this.handleChange.bind(this)
@@ -37,7 +34,6 @@ class MemeGenerator extends Component {
         event.preventDefault()
         const randomNumber = Math.floor(Math.random() * this.state.allMemeImgs.length)
         const randomMemeImg = this.state.allMemeImgs[randomNumber].url
-
         this.setState({randomImg: randomMemeImg})
     }
 
@@ -46,51 +42,22 @@ class MemeGenerator extends Component {
         this.setState({
           deltaPosition: {
             x: x + ui.deltaX,
-            y: y + ui.deltaY,
+            y: y + ui.deltaY
           }
         });
-      };
-
-      adjustXPos = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const {x, y} = this.state.controlledPosition;
-        this.setState({controlledPosition: {x: x - 10, y}});
-      };
-    
-      adjustYPos = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const {controlledPosition} = this.state;
-        const {x, y} = controlledPosition;
-        this.setState({controlledPosition: {x, y: y - 10}});
-      };
-    
-      onControlledDrag = (e, position) => {
-        const {x, y} = position;
-        this.setState({controlledPosition: {x, y}});
-      };
-    
-      onControlledDragStop = (e, position) => {
-        this.onControlledDrag(e, position);
-        this.onStop();
       };
 
     render() {
 
     const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
-    const {deltaPosition, controlledPosition} = this.state;
-
         return (
            <div>
                 
                 <div className="meme">
                 <Draggable {...dragHandlers}>
-                <div className="box">TAK SI UZ KOKOT</div>
+                <div className="box">joz</div>
                 </Draggable>
                     <img src={this.state.randomImg} alt="" />
-                    <h2 className="top">{this.state.topText}</h2>
-                    <h2 className="bottom">{this.state.bottomText}</h2>
                 </div>
                 <div className="container"> 
                 <form className="meme-form" onSubmit={this.handleSubmit}>
